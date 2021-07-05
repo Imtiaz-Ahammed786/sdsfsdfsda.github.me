a62a6737592871a11a3b5d17fae94fdd0cc5b998
Imtiaz Ahammed Nafiz <86876545+Imtiaz-Ahammed786@users.noreply.github.com>
2021-07-05 14:21:23 -0700

    Add files via upload

---
+++ home.js
@@ -0,0 +1,33 @@
+
+
+const man = document.querySelector("#js_connect");
+
+man.addEventListener("click", function(){
+	
+	this.querySelector("i").classList.toggle("fa-times");
+	const program = document.querySelector(".MenuBar");
+	program.classList.toggle("show")
+	
+});
+
+
+
+//We need three lines of JavaScript codes to the preloader
+
+
+window.addEventListener("load", () => {
+	
+	const proloader = document.querySelector(.proloader);
+	proloader.classList.add("proloader-finish");
+	
+});
+
+// I am trying this things right now
+
+
+
+
+ 
+
+
+
